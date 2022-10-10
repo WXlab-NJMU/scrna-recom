0e76a508c6e518718d160223a9043ced0a3646cf
Sue9104 <sumin2012@163.com>
2022-10-10 19:51:19 +0800

    fix: qc output format

--- scrna-recom/R/qc.R
+++ scrna-recom/R/qc.R
@@ -15,7 +15,7 @@ library(dplyr)
 #' @rdname qc
 #' @export
 #'
-qc <- function (indir, outdir, project, 
+qc <- function (indir, outdir, project,
                 max.counts, min.counts, max.genes, min.genes,
                 min.cells, max.mt, max.hb) {
   pbmc.data <- Seurat::Read10X(data.dir = indir)
@@ -29,6 +29,7 @@ qc <- function (indir, outdir, project,
   cat("total gene counts: ", total.genes, "\n")
   pbmc[["percent.mt"]] <- Seurat::PercentageFeatureSet(pbmc, pattern = "^MT-")
   pbmc[["percent.hb"]] <- Seurat::PercentageFeatureSet(pbmc, pattern = "^HB[AB]")
+  pbmc[["percent.rb"]] <- Seurat::PercentageFeatureSet(pbmc, pattern = "^RP[SL]")
   # filt counts
   pbmc <- subset(pbmc, subset = nCount_RNA > min.counts & nCount_RNA < max.counts)
   count.filt = total.cells - ncol(x = pbmc)
@@ -52,33 +53,36 @@ qc <- function (indir, outdir, project,
   cat("final cell counts: ", ncol(pbmc), "\n")
   cat("final gene counts: ", nrow(pbmc), "\n")
   detail <- data.frame(
-    item = c("sample", "total.cells", "total.genes", 
-             "counts.thresold", "counts.filtcells", 
+    item = c("sample", "total.cells", "total.genes",
+             "counts.thresold", "counts.filtcells",
              "genes.thresold", "filtgenes",
              "mt.thresold", "mt.filtcells",
              "hb.thresold", "hb.filtcells",
              "final.cells", "final.genes"),
-    info = c(project, total.cells, total.genes, 
-             count.range, count.filt, 
-             gene.range, gene.filt, 
-             max.mt, mt.filt, 
+    info = c(project, total.cells, total.genes,
+             count.range, count.filt,
+             gene.range, gene.filt,
+             max.mt, mt.filt,
              max.hb, hb.filt,
              ncol(pbmc), nrow(pbmc))
   )
   #detail <- t(detail)
-  write.csv(detail, file.path(outdir, paste0(project,".qc.stat.csv")), 
-            row.names = FALSE, col.names = FALSE, quote = FALSE )
-  return(pbmc) 
+  write.csv(detail, file.path(outdir, paste0(project,".qc.stat.csv")),
+            row.names = FALSE, quote = FALSE)
+  write.table(Seurat::Cells(pbmc),
+              file.path(outdir, paste0(project,".barcodes.csv")),
+              row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "," )
+  return(pbmc)
 }
 
 #' group quality control
-#' 
+#'
 #' @import Seurat
 #' @import dplyr
 #' @rdname qc
 #' @export
 #'
-group_qc <- function (csv, outdir, project, 
+group_qc <- function (csv, outdir, project,
                       max.counts, min.counts, max.genes, min.genes,
                       min.cells, max.mt, max.hb) {
   # create outdir
@@ -96,9 +100,9 @@ group_qc <- function (csv, outdir, project,
     data[["percent.hb"]] <- Seurat::PercentageFeatureSet(data, pattern = "^HB[AB]")
     data
   })
-  raw.data <- merge(sample.data[[1]], tail(sample.data, length(sample.data)-1), 
+  raw.data <- merge(sample.data[[1]], tail(sample.data, length(sample.data)-1),
                     add.cell.ids = samples, project = project)
-  
+
   # plot before qc
   pdf(file.path(outdir, paste0(project,".qc_before.pdf")))
   plot1 <- Seurat::VlnPlot(raw.data, features = c("nFeature_RNA", "nCount_RNA", "percent.mt", "percent.hb"), ncol = 2)
@@ -111,7 +115,7 @@ group_qc <- function (csv, outdir, project,
   plot4 <- Seurat::FeatureScatter(raw.data, feature1 = "nCount_RNA", feature2 = "percent.hb", shuffle=TRUE)
   print(plot4)
   dev.off()
-  # qc 
+  # qc
   sample.qcdata <- apply(inputs, 1, FUN = function(item) {
     indir  <- item[["path"]]
     sample  <- item[["sample"]]
@@ -134,12 +138,12 @@ group_qc <- function (csv, outdir, project,
     print(sample)
     print(c("max.counts", "min.counts", "max.genes", "min.genes", "min.cells", "max.mt", "max.hb"))
     print(c(max.counts, min.counts, max.genes, min.genes, min.cells, max.mt, max.hb))
-    qc(indir, outdir, sample, 
-       as.numeric(max.counts), as.numeric(min.counts), 
-       as.numeric(max.genes), as.numeric(min.genes), 
+    qc(indir, outdir, sample,
+       as.numeric(max.counts), as.numeric(min.counts),
+       as.numeric(max.genes), as.numeric(min.genes),
        as.numeric(min.cells), as.numeric(max.mt), as.numeric(max.hb))
   })
-  qc.data <- merge(sample.qcdata[[1]], tail(sample.qcdata, length(sample.qcdata)-1), 
+  qc.data <- merge(sample.qcdata[[1]], tail(sample.qcdata, length(sample.qcdata)-1),
                     add.cell.ids = samples, project = project)
   # plot after qc
   pdf(file.path(outdir, paste0(project,".qc_after.pdf")))
@@ -155,14 +159,14 @@ group_qc <- function (csv, outdir, project,
   # merge stat
   stat <- do.call(cbind, lapply(samples, function(x) read.csv(file.path(outdir, paste0(x,".qc.stat.csv")))))
   cols <- append(1, seq(1, length(samples))*2 )
-  write.csv(stat[cols], file.path(outdir, paste0(project,".qc.stat.csv")), 
-            row.names = FALSE, quote = FALSE )
-  for (sample in samples){ 
+  write.table(stat[cols], file.path(outdir, paste0(project,".qc.stat.csv")),
+              row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "," )
+  for (sample in samples){
     file.remove(file.path(outdir, paste0(sample,".qc.stat.csv")))
   }
   outrds = file.path(outdir, paste0(project,".qc.rds"))
-  saveRDS(pbmc, outrds)
-  return(pbmc)
+  saveRDS(qc.data, outrds)
+  return(qc.data)
 }
 
 
